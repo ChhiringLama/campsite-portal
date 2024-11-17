cd52679613c5a2e0e3bd74928d379a6bca261abf
rashmi4th <131947291+rashmi4th@users.noreply.github.com>
2024-11-17 15:36:49 +0545

    helper updated

---
+++ seeds/helper.js
@@ -0,0 +1,26 @@
+module.exports.descriptor=[
+    'Forest',
+    'Ancient',
+    'Petrified',
+    'Roaring',
+    'Silent',
+    'Maple',
+    'Misty',
+    'Sea',
+    'Sky',
+    'Diamond',
+    'Cascade'
+]
+
+module.exports.places=[
+    'Flats',
+    'Village',
+    'Canyon',
+    'Pond',
+    'Horse camp',
+    'Mule camp',
+    'Shore',
+    'River',
+    'Hunting camp',
+    'Bay'
+]
